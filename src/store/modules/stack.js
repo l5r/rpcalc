0f587f1283073a4e13981b963db83f1196180075
Leander Lismond <36822732+l5r@users.noreply.github.com>
2018-03-16 15:48:07 +0100

    Add docs to store/modules/stack.js
    to improve readability

--- src/store/modules/stack.js
+++ src/store/modules/stack.js
@@ -1,5 +1,15 @@
 const state = {
+  /**
+   * The items on the stack. Pushed items should be added to the end of the
+   * array.
+   * @type {Array}
+   */
   items: [],
+  /**
+   * Last popped items. When you commit a `pop` mutation the popped items will
+   * be pushed to this array.
+   * @type {Array}
+   */
   popped: []
 }
 
@@ -9,16 +19,38 @@ const getters = {
 }
 
 const mutations = {
+  /**
+   * Adds an item to the top of the stack
+   * @param  {stack.state}  state the state to push to
+   * @param  {Number}       item  the item to push onto the stack
+   */
   push (state, item) {
     state.items.push(item)
   },
+  /**
+   * Pops an item from the stack
+   *
+   * Popped items are returned in state.popped
+   * @param  {stack.state}  state the state to pop from
+   * @param  {Number}       n     amount of items to pop
+   */
   pop (state, n) {
     n = n || 1
     state.popped = state.items.splice(-n, n)
   },
+  /**
+   * Removes an item in the stack
+   * @param  {stack.state}  state state to remove the item from
+   * @param  {Number}       idx   index of the item to remove
+   */
   removeItem (state, idx) {
     state.items.splice(idx, 1)
   },
+  /**
+   * Applies opertor to two items at the top of the stack.
+   * @param  {stack.state}  state state to apply to
+   * @param  {OPERATORS}    op    the operator to apply
+   */
   doCalculation (state, op) {
   }
 }
