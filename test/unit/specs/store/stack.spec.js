6950926d5c20a3cc27fd5ca49ad67ae57c009df5
Leander Lismond <36822732+l5r@users.noreply.github.com>
2018-03-16 15:20:58 +0100

    Add tests for 'store/modules/stack.js'
    Testing is good.
    
    These will also be used when moving the calculator logic to the vuex
    store.

---
+++ test/unit/specs/store/stack.spec.js
@@ -0,0 +1,77 @@
+import stack from '@/store/modules/stack'
+
+const {state, getters, mutations} = stack
+
+describe('store/stack.js', () => {
+  it('starts with an empty state', () => {
+    // This test is here so we don't forget to update the other tests if we
+    // ever change the default state, also don't forget to also update other
+    // parts of the code that assume this state.
+    expect(state).to.eql({
+      items: [],
+      popped: []
+    })
+  })
+
+  describe('getters', () => {
+    it('returns sensible defaults with an empty state', () => {
+      expect(getters.stackItems({})).to.deep.equal([])
+    })
+  })
+
+  describe('mutations', () => {
+    it('rejects NaN to be pushed', () => {
+      expect(() => mutations.push(state, NaN)).to.throw()
+    })
+
+    it('adds items to the stack', () => {
+      let _state = {items: [0]}
+
+      mutations.push(_state, 1)
+
+      expect(_state.items).to.include(1)
+    })
+
+    it('pops items from the stack', () => {
+      let _state = {
+        items: [0, 1, 2, 3],
+        popped: []
+      }
+
+      mutations.pop(_state)
+      expect(_state.popped).to.deep.equal([3])
+
+      mutations.pop(_state, 2)
+      expect(_state.popped).to.deep.equal([1, 2])
+    })
+
+    it('maintains a fifo order', () => {
+      let _state = {
+        items: [0, 1, 2, 3],
+        popped: []
+      }
+
+      mutations.push(_state, 10)
+      mutations.push(_state, 11)
+      mutations.push(_state, 12)
+      expect(_state.items).to.eql([0, 1, 2, 3, 10, 11, 12])
+
+      mutations.pop(_state)
+      expect(_state.items).to.eql([0, 1, 2, 3, 10, 11])
+      expect(_state.popped).to.eql([12])
+
+      mutations.pop(_state, 3)
+      expect(_state.items).to.eql([0, 1, 2])
+      expect(_state.popped).to.eql([3, 10, 11])
+    })
+
+    it('removes items', () => {
+      let _state = {
+        items: [0, 10, 20, 30, 40]
+      }
+
+      mutations.removeItem(_state, 2)
+      expect(_state.items).to.eql([0, 10, 30, 40])
+    })
+  })
+})
